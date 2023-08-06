86f2e1ca7416063a278f3e6b54067f61e5d24bf9
Juan Kenichi Sutan <kenichi.jks@gmail.com>
2023-08-06 20:09:07 +0900

    Minor information change

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,11 @@ const Home = () => {
     return(
         <>
             <div className="text-center">
-                <h2>Course Schedule Management</h2>
+                <h2>Home</h2>
                 <hr />
+            </div>
+            <div className="text-left">
+                <h4>Select a function from the left tab to continue</h4>
             </div>
         </>
     )
