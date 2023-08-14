8a1c24b82a702bac94a4930bde8b12ef2b0c0328
Juan Kenichi Sutan <kenichi.jks@gmail.com>
2023-08-15 00:22:09 +0900

    Courses now uses data provided by JSON files

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -1,30 +1,14 @@
 import {useEffect, useState} from "react";
 import {Link} from "react-router-dom";
+import {timetableDataFall} from "../data/TimetableDataFall";
 
-const Games = () => {
+const Courses = () => {
     const [courses, setCourses] = useState([]);
 
     useEffect(() => {
-        const data = [
-            {
-                id: 1,
-                departmentTitle: "Computer and Information Science",
-                department: "CIS",
-                number: 1051,
-                section: 801,
-                title: "Computer Programming in C",
-                credits: 4,
-                day: "MWF",
-                startTime: "10:00",
-                endTime: "11:20",
-                instructor: "Karam, H",
-                crn: 52434,
-                genEd: null,
-                prior: null,
-                info: null,
-            },
-        ]
-        setCourses(data)
+        // Convert object to array and set it as courses
+        const timetableDataFallArray = Object.values(timetableDataFall);
+        setCourses(timetableDataFallArray);
     }, [])
 
     return(
@@ -51,11 +35,11 @@ const Games = () => {
                     {courses.map( (course) =>
                         <tr key={course.id}>
                             <td>
-                                <Link to={`/department/${course.department}`}>{course.departmentTitle}</Link>
+                                <Link to={`/department/${course.department}`}>{course.subject}</Link>
                             </td>
                             <td>{course.department} {course.number}</td>
                             <td>
-                                <Link to={`/course/${course.id}`}>{course.title}</Link>
+                                <Link to={`/course/${course.id}`}>{course.courseName}</Link>
                             </td>
                             <td>{course.credits}</td>
                             <td>{course.day} {course.startTime} - {course.endTime}</td>
@@ -73,4 +57,4 @@ const Games = () => {
     )
 }
 
-export default Games;
+export default Courses;
